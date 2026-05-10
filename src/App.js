34456906ead080d94298784b37d3a85ef99d8327
Suraj Kumar <skumar8595304933@gmail.com>
2026-05-10 23:14:56 +0530

    Fix duplicate signup route in App.js
    Removed duplicate signup route.

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
         <Route path="/" element={<Home/>} />
         <Route path="/login" element={<Login/>} />
         <Route path="/signup" element={<Signup/>} />
+        <Route path="/signup" element={<Signup/>} />
       </Routes>
     </div>
   );
